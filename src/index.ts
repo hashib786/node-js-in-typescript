6fa7a090e4f4adaa7830dc164b51a2bb6a4f8da9
Hashib Raja <hashibraja23@gmail.com>
2023-08-03 19:58:53 +0530

    Creating server using http module

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import fs from "node:fs";
-import { join } from "node:path";
+import http from "node:http";
+import { URL } from "node:url";
+// import fs from "node:fs";
+// import { join } from "node:path";
 
 // console.log("first");
 // const hello: string = "Hello";
@@ -15,14 +17,74 @@ import { join } from "node:path";
 // console.log(fs.readFileSync("../txt/input.txt", "utf8"));
 
 // Using Asycronus way
-const pathName = join(process.cwd(), "/txt/input.txt");
+// const pathName = join(process.cwd(), "/txt/input.txt");
 
-const readingFIleFunc = (
-  err: NodeJS.ErrnoException | null,
-  data: string
-): void => {
-  if (err) return console.log("Error reading input file", err);
-  console.log(data);
-};
+// const readingFIleFunc = (
+//   err: NodeJS.ErrnoException | null,
+//   data: string
+// ): void => {
+//   if (err) return console.log("Error reading input file", err);
+//   console.log(data);
+// };
 
-fs.readFile(pathName, "utf-8", readingFIleFunc);
+// fs.readFile(pathName, "utf-8", readingFIleFunc);
+// fs.unlink(join(process.cwd(), "/txt/final.txt"), (err) => {
+//   console.log(err);
+// });
+
+// fs.exists(join(process.cwd(), "/txt/final.txt"), (exist) => {
+//   console.log(exist);
+// });
+
+//  For creating Basic Server
+const server = http.createServer(
+  (req: http.IncomingMessage, res: http.ServerResponse) => {
+    res.writeHead(200, "Hello World Hashib", {
+      "Content-Type": "text/html",
+    });
+    // console.log(req);
+    res.end(`
+    <h1>Hello Saba</h1>
+    <script>console.log("req")</script>
+    `);
+
+    // Overview page
+    // if (pathname === '/' || pathname === '/overview') {
+    //   res.writeHead(200, {
+    //     'Content-type': 'text/html'
+    //   });
+
+    //   const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    //   const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    //   res.end(output);
+
+    //   // Product page
+    // } else if (pathname === '/product') {
+    //   res.writeHead(200, {
+    //     'Content-type': 'text/html'
+    //   });
+    //   const product = dataObj[query.id];
+    //   const output = replaceTemplate(tempProduct, product);
+    //   res.end(output);
+
+    //   // API
+    // } else if (pathname === '/api') {
+    //   res.writeHead(200, {
+    //     'Content-type': 'application/json'
+    //   });
+    //   res.end(data);
+
+    //   // Not found
+    // } else {
+    //   res.writeHead(404, {
+    //     'Content-type': 'text/html',
+    //     'my-own-header': 'hello-world'
+    //   });
+    //   res.end('<h1>Page not found!</h1>');
+    // }
+  }
+);
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
